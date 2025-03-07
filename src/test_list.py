020c9c7364d40f451586bf09ebc9721998888ec5
dk872 <dimakulyk2005@gmail.com>
2025-03-07 14:21:04 +0200

    Add tests for length method

--- src/test_list.py
+++ src/test_list.py
@@ -5,3 +5,20 @@ from list_class import List
 class TestListMethods(unittest.TestCase):
     def setUp(self):
         self.lst = List()
+
+    def test_length_empty(self):
+        """Checks that the length of an empty list is calculated correctly."""
+        self.assertEqual(self.lst.length(), 0)
+
+    def test_length_after_adding(self):
+        """Checks that the length of the list is calculated correctly after adding elements."""
+        self.lst.append('a')
+        self.lst.append('b')
+        self.assertEqual(self.lst.length(), 2)
+
+    def test_length_after_removing(self):
+        """Checks that the length of a list is calculated correctly after adding and removing elements."""
+        self.lst.append('a')
+        self.lst.append('b')
+        self.lst.delete(0)
+        self.assertEqual(self.lst.length(), 1)
